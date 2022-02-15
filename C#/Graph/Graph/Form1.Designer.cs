c3fcefc22be32d5fa6425edbfd65aa0ec63ac763
HpPpL <enderloshka38@gmail.com>
2022-02-15 17:53:35 +0300

    My projects

---
+++ C#/Graph/Graph/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Graph
+{
+    partial class Frm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl = new System.Windows.Forms.Panel();
+            this.btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.per_a = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.per_b = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.per_c = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.per_d = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.clr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnl
+            // 
+            this.pnl.BackColor = System.Drawing.Color.Moccasin;
+            this.pnl.Location = new System.Drawing.Point(12, 12);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(600, 600);
+            this.pnl.TabIndex = 0;
+            this.pnl.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl_Paint);
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(706, 327);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(191, 83);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Построить график";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(702, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Коэффициент A:";
+            // 
+            // per_a
+            // 
+            this.per_a.Location = new System.Drawing.Point(706, 85);
+            this.per_a.Name = "per_a";
+            this.per_a.Size = new System.Drawing.Size(143, 20);
+            this.per_a.TabIndex = 3;
+            this.per_a.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label2.Location = new System.Drawing.Point(704, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Коэффициент B:";
+            // 
+            // per_b
+            // 
+            this.per_b.Location = new System.Drawing.Point(706, 145);
+            this.per_b.Name = "per_b";
+            this.per_b.Size = new System.Drawing.Size(142, 20);
+            this.per_b.TabIndex = 5;
+            this.per_b.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.Location = new System.Drawing.Point(703, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Коэффициент C:";
+            // 
+            // per_c
+            // 
+            this.per_c.Location = new System.Drawing.Point(706, 205);
+            this.per_c.Name = "per_c";
+            this.per_c.Size = new System.Drawing.Size(142, 20);
+            this.per_c.TabIndex = 7;
+            this.per_c.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label4.Location = new System.Drawing.Point(704, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Коэффициент D:";
+            // 
+            // per_d
+            // 
+            this.per_d.Location = new System.Drawing.Point(708, 275);
+            this.per_d.Name = "per_d";
+            this.per_d.Size = new System.Drawing.Size(142, 20);
+            this.per_d.TabIndex = 9;
+            this.per_d.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(635, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(270, 38);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "График:|||A*x*x+B*x|-C|-D|";
+            // 
+            // clr
+            // 
+            this.clr.Location = new System.Drawing.Point(704, 432);
+            this.clr.Name = "clr";
+            this.clr.Size = new System.Drawing.Size(193, 82);
+            this.clr.TabIndex = 11;
+            this.clr.Text = "Отчистить";
+            this.clr.UseVisualStyleBackColor = true;
+            this.clr.Click += new System.EventHandler(this.clr_Click);
+            // 
+            // Frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(992, 749);
+            this.Controls.Add(this.clr);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.per_d);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.per_c);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.per_b);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.per_a);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.pnl);
+            this.Name = "Frm";
+            this.Text = "График";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox per_a;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox per_b;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox per_c;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox per_d;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button clr;
+    }
+}
+
---
+++ C#/Graph/Graph/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graph
+{
+    public partial class Frm : Form
+    {
+        double f(double x, double a, double b, double c, double d)
+        {
+
+            double y = Math.Abs(Math.Abs(Math.Abs(a * x * x + b* x) - c) - d);
+            return y;
+        }
+        public Frm()
+        {
+            InitializeComponent();
+        }
+
+        private void pnl_Paint(object sender, PaintEventArgs e)
+        {
+            var psk = e.Graphics;
+
+            //Рисуем систему координат
+            using (Pen pen = new Pen(Color.Aqua, 2))
+            {
+
+                Font my_font = new Font("Mistral", 14, FontStyle.Bold);
+                Font my_font_numb = new Font("Mistral", 8, FontStyle.Bold);
+                int pnl_weight = 600;
+                //Рисуем ось
+                for (int i = 0; i <= pnl_weight; i += 20)
+                {
+                    psk.DrawLine(new Pen(Color.LightSlateGray, 1), 0, i, pnl_weight, i);
+                    psk.DrawLine(new Pen(Color.LightSlateGray, 1), i, 0, i, pnl_weight);
+
+                }
+                psk.DrawLine(new Pen(Color.LightSlateGray, 1), 599, 0, 599, 599);
+                psk.DrawLine(new Pen(Color.LightSlateGray, 1), 0, 599, 599, 599);
+                //Рисуем штриховку
+                for (int i = 20; i <= pnl_weight; i += 20)
+                {
+                    psk.DrawLine(new Pen(Color.Black, 1), (pnl_weight / 2) - 5, i, (pnl_weight / 2) + 5, i);
+                    psk.DrawLine(new Pen(Color.Black, 1), i, (pnl_weight / 2) - 5, i, (pnl_weight / 2) + 5);
+                }
+                //Нумеруем
+                int k1, k2, k3, k4;
+                k1 = -1;
+                k2 = 1;
+                k3 = 1;
+                k4 = -1;
+                for (int i = pnl_weight / 2 + 20; i < pnl_weight; i += 20)
+                {
+                    psk.DrawString("/4", my_font_numb, Brushes.Black, (pnl_weight / 2) + 25, i - 10);
+                    k1--;
+                    psk.DrawString(k1.ToString(), my_font_numb, Brushes.Black, (pnl_weight / 2) + 10, i - 10);
+                    k1--;
+
+                }
+
+                for (int i = pnl_weight / 2 + 20; i < pnl_weight; i += 20)
+                {
+                    psk.DrawString("4", my_font_numb, Brushes.Black, i - 5, (pnl_weight / 2) - 13);
+                    psk.DrawString("--", my_font_numb, Brushes.Black, i - 5, (pnl_weight / 2) - 18);
+                    k2++;
+                    psk.DrawString(k2.ToString(), my_font_numb, Brushes.Black, i - 5, (pnl_weight / 2) - 23);
+                    k2++;
+                }
+
+
+                for (int i = pnl_weight / 2; i > 20; i -= 20)
+                {
+                    psk.DrawString("4", my_font_numb, Brushes.Black, i - 25, (pnl_weight / 2) + 23);
+                    psk.DrawString("--", my_font_numb, Brushes.Black, i - 25, (pnl_weight / 2) + 16);
+                    k4--;
+                    psk.DrawString(k4.ToString(), my_font_numb, Brushes.Black, i - 30, (pnl_weight / 2) + 10);
+                    k4--;
+                }
+
+                for (int i = pnl_weight / 2; i > 20; i -= 20)
+                {
+                    psk.DrawString("/4", my_font_numb, Brushes.Black, (pnl_weight / 2) - 20, i - 30);
+                    k3++;
+                    psk.DrawString(k3.ToString(), my_font_numb, Brushes.Black, (pnl_weight / 2) - 30, i - 30);
+                    k3++;
+                }
+                // Рисуем ось абсцисс и ординат
+                psk.DrawLine(new Pen(Color.Gray, 2), pnl_weight, pnl_weight / 2, 0, pnl_weight / 2);
+                psk.DrawLine(new Pen(Color.Gray, 2), pnl_weight / 2, pnl_weight, pnl_weight / 2, 0);
+
+                //Подписываем оси и ставим стрелки
+                Pen blackPen = new Pen(Color.Black, 1);
+                PointF point1 = new PointF(590, 295);
+                PointF point2 = new PointF(600, 300);
+                PointF point3 = new PointF(590, 305);
+                PointF[] curvePoints =
+                {
+                    point1,
+                    point2,
+                    point3,
+                };
+
+                psk.DrawPolygon(new Pen(Color.Gray, 2), curvePoints);
+
+
+                PointF point4 = new PointF(295, 10);
+                PointF point5 = new PointF(300, 0);
+                PointF point6 = new PointF(305, 10);
+                PointF[] curvePointss =
+                 {
+                     point4,
+                     point5,
+                     point6,
+                 };
+                psk.DrawPolygon(new Pen(Color.Gray, 2), curvePointss);
+
+
+
+                psk.DrawString("X", my_font, Brushes.Red, 585, 310);
+                psk.DrawString("Y", my_font, Brushes.Red, 310, 0);
+            }
+
+        }
+
+
+
+        void btn_Click(object sender, EventArgs e)
+        {
+            //Строим график
+            int pnl_weight = 600;
+            Bitmap pen = new Bitmap(1, 1);
+            pen.SetPixel(0, 0, Color.Black);
+            double a = Convert.ToDouble(per_a.Text);
+            double b = Convert.ToDouble(per_b.Text);
+            double c = Convert.ToDouble(per_c.Text);
+            double d = Convert.ToDouble(per_d.Text);
+            double x, y, xp = -10, yp = f(xp, a, b, c, d), i;
+            // double x_max=5, x_min=-5;
+            System.Drawing.Graphics g = pnl.CreateGraphics();
+            for (i = -15; i <= 14; i += 0.001D)
+            {
+                x = i;
+                y = f(x, a, b, c, d) * 2;
+                g.DrawLine(new Pen(Color.Black, 2), Convert.ToInt32((xp * 40 + pnl_weight / 2)), Convert.ToInt32(pnl_weight / 2 - yp * 20), Convert.ToInt32((x * 40 + pnl_weight / 2)), Convert.ToInt32(pnl_weight / 2 - y * 20));
+                xp = x;
+                yp = y;
+            }
+        }
+
+        private void clr_Click(object sender, EventArgs e)
+        {
+            var psk = pnl.CreateGraphics();
+            psk.Clear(Color.Moccasin);
+            //Рисуем систему координат
+            using (Pen pen = new Pen(Color.Aqua, 2))
+            {
+
+                Font my_font = new Font("Mistral", 14, FontStyle.Bold);
+                Font my_font_numb = new Font("Mistral", 8, FontStyle.Bold);
+                int pnl_weight = 600;
+                //Рисуем ось
+                for (int i = 0; i <= pnl_weight; i += 20)
+                {
+                    psk.DrawLine(new Pen(Color.LightSlateGray, 1), 0, i, pnl_weight, i);
+                    psk.DrawLine(new Pen(Color.LightSlateGray, 1), i, 0, i, pnl_weight);
+
+                }
+                psk.DrawLine(new Pen(Color.LightSlateGray, 1), 599, 0, 599, 599);
+                psk.DrawLine(new Pen(Color.LightSlateGray, 1), 0, 599, 599, 599);
+                //Рисуем штриховку
+                for (int i = 20; i <= pnl_weight; i += 20)
+                {
+                    psk.DrawLine(new Pen(Color.Black, 1), (pnl_weight / 2) - 5, i, (pnl_weight / 2) + 5, i);
+                    psk.DrawLine(new Pen(Color.Black, 1), i, (pnl_weight / 2) - 5, i, (pnl_weight / 2) + 5);
+                }
+                //Нумеруем
+                int k1, k2, k3, k4;
+                k1 = -1;
+                k2 = 1;
+                k3 = 1;
+                k4 = -1;
+                for (int i = pnl_weight / 2 + 20; i < pnl_weight; i += 20)
+                {
+                    psk.DrawString("/4", my_font_numb, Brushes.Black, (pnl_weight / 2) + 25, i - 10);
+                    k1--;
+                    psk.DrawString(k1.ToString(), my_font_numb, Brushes.Black, (pnl_weight / 2) + 10, i - 10);
+                    k1--;
+
+                }
+
+                for (int i = pnl_weight / 2 + 20; i < pnl_weight; i += 20)
+                {
+                    psk.DrawString("4", my_font_numb, Brushes.Black, i - 5, (pnl_weight / 2) - 13);
+                    psk.DrawString("--", my_font_numb, Brushes.Black, i - 5, (pnl_weight / 2) - 18);
+                    k2++;
+                    psk.DrawString(k2.ToString(), my_font_numb, Brushes.Black, i - 5, (pnl_weight / 2) - 23);
+                    k2++;
+                }
+
+
+                for (int i = pnl_weight / 2; i > 20; i -= 20)
+                {
+                    psk.DrawString("4", my_font_numb, Brushes.Black, i - 25, (pnl_weight / 2) + 23);
+                    psk.DrawString("--", my_font_numb, Brushes.Black, i - 25, (pnl_weight / 2) + 16);
+                    k4--;
+                    psk.DrawString(k4.ToString(), my_font_numb, Brushes.Black, i - 30, (pnl_weight / 2) + 10);
+                    k4--;
+                }
+
+                for (int i = pnl_weight / 2; i > 20; i -= 20)
+                {
+                    psk.DrawString("/4", my_font_numb, Brushes.Black, (pnl_weight / 2) - 20, i - 30);
+                    k3++;
+                    psk.DrawString(k3.ToString(), my_font_numb, Brushes.Black, (pnl_weight / 2) - 30, i - 30);
+                    k3++;
+                }
+                // Рисуем ось абсцисс и ординат
+                psk.DrawLine(new Pen(Color.Gray, 2), pnl_weight, pnl_weight / 2, 0, pnl_weight / 2);
+                psk.DrawLine(new Pen(Color.Gray, 2), pnl_weight / 2, pnl_weight, pnl_weight / 2, 0);
+
+                //Подписываем оси и ставим стрелки
+                Pen blackPen = new Pen(Color.Black, 1);
+                PointF point1 = new PointF(590, 295);
+                PointF point2 = new PointF(600, 300);
+                PointF point3 = new PointF(590, 305);
+                PointF[] curvePoints =
+                {
+                    point1,
+                    point2,
+                    point3,
+                };
+
+                psk.DrawPolygon(new Pen(Color.Gray, 2), curvePoints);
+
+
+                PointF point4 = new PointF(295, 10);
+                PointF point5 = new PointF(300, 0);
+                PointF point6 = new PointF(305, 10);
+                PointF[] curvePointss =
+                 {
+                     point4,
+                     point5,
+                     point6,
+                 };
+                psk.DrawPolygon(new Pen(Color.Gray, 2), curvePointss);
+
+
+
+                psk.DrawString("X", my_font, Brushes.Red, 585, 310);
+                psk.DrawString("Y", my_font, Brushes.Red, 310, 0);
+            }
+        }
+    }
+}
